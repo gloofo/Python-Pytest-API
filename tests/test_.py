74ed230e3cdc855c9e9d8b0728cd65563e08ea9e
gloofo <gloofo@gloofo.glof>
2023-07-08 19:15:11 +0800

    Rearranged

--- tests/test_.py
+++ tests/test_.py
@@ -1,4 +1,10 @@
-from src.modules import *
+import os
+import sys
+
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(src_path)
+
+from modules import *
 
 endpoint = "https://reqres.in"
 
@@ -15,9 +21,9 @@ def test_CreateUser():
     global getData
     response = postRequest(path="single", dataPayload=payload())
     getData = response.json()
-    print(getData['id'])
     assert response.status_code == 201
 
+#delete user using the id of the created use
 def test_DeleteUser():
     response = delRequest(path="single", userId=f"{getData['id']}")
     assert response.status_code == 204
@@ -26,9 +32,9 @@ def test_DeleteUser():
 def test_registerUser():
     dataPass = jsonData(password=fake().password())
     response = postRequest(path="register", dataPayload=dataPass)
-    assert response.status_code == 400
     #response should be 200, but their website is returning 400
     #I also tried using POSTMAN API same response.
+    assert response.status_code == 400
     
 #returns json payload
 def payload():
